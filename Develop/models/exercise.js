ac033bfe703e0a9c9e36d28a05f43e07183e67b9
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-09-17 23:46:06 -0700

    added exercises array in model schema

--- Develop/models/exercise.js
+++ Develop/models/exercise.js
@@ -6,32 +6,38 @@ const excercise = new Schema({
     type: Date,
     default: Date.now,
   },
-  type: {
-    type: String,
-    trim: true,
-    required: "Strength Training or Cardio?",
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "What exercise?",
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  weight: {
-    type: Number,
-    required: true,
-  },
-  reps: {
-    type: Number,
-    required: true,
-  },
-  sets: {
-    type: Number,
-    required: true,
-  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Strength Training or Cardio?",
+      },
+    
+      name: {
+        type: String,
+        trim: true,
+        required: "What exercise?",
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        required: true,
+      },
+      sets: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
 });
 
 const Excercise = mongoose.model("Exercise", excercise);
