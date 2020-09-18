8c55340fa6b4480d6312863a23f2a4303dca92be
Jeremiah Yates <jeremiahyates1992@gmail.com>
2020-09-17 23:47:21 -0700

    add new database url

--- Develop/server.js
+++ Develop/server.js
@@ -13,11 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/workout-routines";
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database connected...");
+  })
+  .catch((error) => console.log(error));
 
 // require("./routes/router.js")(app);
 
@@ -33,12 +39,23 @@ app.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
-app.post("/api/workouts", function (req, res) {
+app.post("/api/add-exercise", function (req, res) {
   console.log("posting to workouts", req);
+  Data.Exercise.update(
+    { _id: req.body.id },
+    { $push: { exercises: req.body.exercises } },
+    (err) => {
+      if (err) return res.status(500).json(err);
+      res.json("Exercise added to workout");
+    }
+  );
+});
+
+app.post("/api/add-workout", function (req, res) {
   const newExercise = new Data.Exercise(req.body);
   newExercise.save(function (err) {
     if (err) return res.status(500).json(err);
-    res.json("its lit");
+    res.json("New workout created");
   });
 });
 
@@ -48,8 +65,13 @@ app.post("/api/workouts/bulk", function (req, res) {
 });
 
 app.get("/api/workouts", function (req, res) {
-  console.log("getting from workouts");
-  res.json("its lit");
+  Data.Exercise.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 app.get("/api/workouts/range", (req, res) => {
